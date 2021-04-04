71bf7295c2c909709a17518f5ca26e31e1e69308
eliott mogenet <eliott.mogenet@gmail.com>
2021-04-04 16:23:12 +0200

    4 landing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/calculator' => 'pages#calculator'
+  get '/new' => 'pages#new'
+  get '/usa' => 'pages#usa'
+  get '/sales_fr' => 'pages#sales_fr'
+  get '/cfo' => 'pages#cfo'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
